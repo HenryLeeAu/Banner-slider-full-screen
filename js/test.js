e4d321998c7a78f94aa0a459fd0f77d5ee94ae25
HanweiLee <adria.0910@gmail.com>
2016-03-03 07:27:52 +0000

    modify  js

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@
 
 	$.fn.julio = function(options) {
 		var defaults = {
-				 'bannerNum' :-2,
+				 'bannerNum' :2,
 				 'duration' :5000,
 				 'auto' :'on',
 				 'ball' :'off'
@@ -25,57 +25,53 @@
 					$(this).children(".banner_point").append( "<li class='"+i+"'></li>" );
 		  		}
 		  	}
+		  	
 			init(bannerNum);
-			if(bannerNum==0){
-        		$(that).children(".arrow_l").addClass('hidden');
-      		}else if(bannerNum==-(bannerLength-1)){
-        		$(that).children(".arrow_r").addClass('hidden');
-      		}
-			bannerMove(bannerNum);
-			
+
   			function bannerMove(num){
-  				$(that).children('.banner_wrap').animate({marginLeft:$(window).width()*num+"px"}, 1000, function(){
-  					$(that).children(".arrow_l").removeClass('hidden');
-      				$(that).children(".arrow_r").removeClass('hidden');
-      				if(bannerNum==0){
-        				$(that).children(".arrow_l").addClass('hidden');
-      				}else if(bannerNum==-(bannerLength-1)){
-        				$(that).children(".arrow_r").addClass('hidden');
-      				}
-  				});
+  				$(that).children('.banner_wrap').animate({marginLeft:$(window).width()*-num+"px"}, 1000, ButtonStatus());
     		}
-    		
     		 $(window).resize(function(){
         		init(bannerNum);
   			});
     		
-		 
+		 	function ButtonStatus(){
+      			$(that).children(".arrow_l").removeClass('hidden');
+      			$(that).children(".arrow_r").removeClass('hidden');
+      			if(bannerNum==0){
+        			$(that).children(".arrow_l").addClass('hidden');
+      			}else if(bannerNum==bannerLength-1){
+        			$(that).children(".arrow_r").addClass('hidden');
+      			}
+      		}
+
 		  	$(this).children(".arrow_l").click(function(){
-		  	 	bannerNum++;
-   				 if(bannerNum>=0){
-      				bannerNum=0;
+		  	 	bannerNum--;
+   				 if(bannerNum < 0){
+      				bannerNum = 0;
     			}
     			
     			 ClocknMove(bannerNum);
 		  	 });
 		  	$(this).children(".arrow_r").click(function(){
-		  	 	bannerNum--;
-   				  if(bannerNum<-(bannerLength-1)){
-      				bannerNum=-(bannerLength-1); 
+		  	 	bannerNum++;
+   				  if(bannerNum > bannerLength-1){
+      				bannerNum = bannerLength-1; 
     			}
     			
     			 ClocknMove(bannerNum);
 		  	});
 		  	$(this).children(".banner_point").children("li").click(function(e){
-      			bannerNum=-$(e.target).attr("class");
+      			bannerNum=$(e.target).attr("class");
       			
       			ClocknMove(bannerNum);
+      			
   			});
   			function init(num){
     			now_width=$(window).width();
-
+    			
    				$(that).children(".banner_wrap").children(".banner").css({"width":now_width+'px'});
-    			$(that).children(".banner_wrap").css({"width":now_width*$(".banner").length+'px',"margin-left":now_width*num+"px"});
+    			$(that).children(".banner_wrap").css({"width":now_width*$(".banner").length+'px',"margin-left":-now_width*num+"px"});
        			
        		}
 
@@ -89,9 +85,9 @@
 			}
 			function autoslide(){
 				
-    			bannerNum--;
+    			bannerNum++;
     		
-    			if(bannerNum<=-bannerLength){
+    			if(bannerNum>bannerLength-1){
       				bannerNum=0;
     			}
     			bannerMove(bannerNum);
